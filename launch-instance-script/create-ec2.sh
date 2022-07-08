f990319f90d7aa045feab77b04fa3320855178c6
00516Q744 <manojamreddy@gmail.com>
2022-07-08 11:48:13 +0530

    changed AMI value in ec2 script

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -27,7 +27,7 @@ create_ec2() {
 }
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-AMI_ID="ami-0eccf89e870877654"
+AMI_ID="ami-08f86308a1e615780"
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 if [ "$COMPONENT" == "all" ]; then
